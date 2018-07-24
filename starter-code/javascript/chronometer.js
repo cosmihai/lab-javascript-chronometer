d3699ee1e2ccbfa47c0ff34b702726af7c4778b3
mihai <cos_mihai23@yahoo.com>
2018-07-24 23:57:58 +0200

    BCN -Mihai

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -5,18 +5,21 @@
 }
 
 Chronometer.prototype.startClick = function () {
+  var that = this;
   this.intervalId = setInterval(function() {
-    this.currentTime++;
-  }, 1000);
+    that.currentTime++;
+    that.setTime();
+  }, 10);
 };
 
 Chronometer.prototype.setMinutes = function () {
-  var minutes =Math.floor(this.currentTime/60 );
+  var minutes = Math.floor(this.currentTime/6000 );
   return minutes;
 };
 
  Chronometer.prototype.setSeconds = function () {
-  var seconds = this.currentTime%60; 
+  var totalSeconds = Math.floor(this.currentTime/100 ); 
+  var seconds = totalSeconds%60;
   return seconds;
 };
 
@@ -25,21 +28,29 @@ Chronometer.prototype.twoDigitsNumber = function (number) {
 };
 
 Chronometer.prototype.setTime = function () {
-
+  document.getElementById('minDec').innerText = this.twoDigitsNumber(this.setMinutes())[0];
+  document.getElementById('minUni').innerText = this.twoDigitsNumber(this.setMinutes())[1];
+  document.getElementById('secDec').innerText = this.twoDigitsNumber(this.setSeconds())[0];
+  document.getElementById('secUni').innerText = this.twoDigitsNumber(this.setSeconds())[1];
+  document.getElementById('milDec').innerText = this.twoDigitsNumber(this.setMilliseconds())[0];
+  document.getElementById('milUni').innerText = this.twoDigitsNumber(this.setMilliseconds())[1];
 };
 
-// Chronometer.prototype.setMilliseconds = function () {
+Chronometer.prototype.setMilliseconds = function () {
+  var miliseconds = Math.floor(this.currentTime%100 ); 
+  return miliseconds;
+};
 
-// };
+Chronometer.prototype.stopClick = function () {
+  clearInterval(this.intervalId);
+  this.intervalId=0;
+};
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.resetClick = function () {
+  this.currentTime = 0;
+  this.intervalId=0;
+};
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+Chronometer.prototype.splitClick = function () {
+  return this.currentTime
+};
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -7,54 +7,86 @@ var secDec      = document.getElementById('secDec');
 var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
+var splitsParent= document.getElementById('splits');
+
+
+var on = false;
 
 
 function printTime() {
-
+  return printMinutes()+':'+printSeconds()+':'+printMilliseconds();
 }
 
 function printMinutes() {
-
+  var min = minDec.innerText+minUni.innerText;
+  return min;
 }
 
 function printSeconds() {
-
+  var sec = secDec.innerText+secUni.innerText;
+  return sec;
 }
 
 function printMilliseconds() {
-
+  var milisec = milDec.innerText+milUni.innerText;
+  return milisec;
 }
 
 function printSplit() {
+  var split = document.createElement('li');
 
+  splitsParent.appendChild(split);
+  split.innerText = printTime();
 }
 
 function clearSplits() {
-
+  splitsParent.innerHTML = '';
 }
 
 function setStopBtn() {
-
+  chronometer.stopClick();
+  on = !on;
+  btnLeft.innerText = "START";
+  btnRight.innerText = "RESET";
 }
 
 function setSplitBtn() {
-
+  printSplit();
 }
 
 function setStartBtn() {
-
+  time = chronometer.startClick();
+  on = !on;
+  btnLeft.innerText = "STOP";
+  btnRight.innerText = "SPLIT";
 }
 
 function setResetBtn() {
+  clearSplits();
+  chronometer.resetClick();
+  minDec.innerText = '0';
+  minUni.innerText = '0';
+  secDec.innerText = '0';
+  secUni.innerText = '0';
+  milDec.innerText = '0';
+  milUni.innerText = '0';
 
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+  if(!on) {
+    setStartBtn();
+  }else {
+    setStopBtn();
+  }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+  if(on) {
+    setSplitBtn();
+  }else {
+    setResetBtn();
+  }
 });
